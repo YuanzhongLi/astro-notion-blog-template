615160653f97754e836c271466290a724ce2be4e
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2024-08-07 14:54:06 +0900

    issue1

--- src/slug_to_pageid.ts
+++ src/slug_to_pageid.ts
@@ -2,12 +2,7 @@ interface SlugPageIdMapping {
   [key: string]: string | undefined
 }
 
+// This is for hard code mapping of slug to page id
 export const SLUG_PAGE_ID_MAPPING: SlugPageIdMapping = {
-  'customize-zhong-notion-blog': 'c209d20a140446e1a8c0d116bc10a4d7',
-  'ist-ci-2023S-programming': 'b47562e419164f849cb0fddb0113f865',
-  'australia-visa-600': 'e23b0bd4bb004afdb1dcfbac9cfcab21',
-  'harumi-flag-1month': '2dec4611e3374b1ebc4ee377dcb55c40',
-  'test-page': '96dda59c5bd3417c9e68d1e3849f1df0',
-  'supported-blocks': '5e49332da86d4dea99ad6d852f57a9c9',
-  'what-is-astro-notion-blog': 'c9a90f296aba485f84c247f0c7ded822',
+
 };
